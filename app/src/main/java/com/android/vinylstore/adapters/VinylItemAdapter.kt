008d72887bcd5e98cb0217b88bb2b7966287c7df
Vladislav Gladkiy <v.a.gladkiy1@gmail.com>
2022-07-02 19:35:44 +0300

    Use ViewBinding in VinylItemAdapter.kt

--- app/src/main/java/com/android/vinylstore/adapters/VinylItemAdapter.kt
+++ app/src/main/java/com/android/vinylstore/adapters/VinylItemAdapter.kt
@@ -7,25 +7,19 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.android.vinylstore.R
+import com.android.vinylstore.databinding.VinylItemBinding
 import com.android.vinylstore.model.Vinyl
 
 class VinylItemAdapter(private val dataSet: Array<Vinyl>) : RecyclerView.Adapter<VinylItemAdapter.ViewHolder>() {
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val vinylNameTextView: TextView
-        val vinylAuthorTextView: TextView
-        val vinylImageView: ImageView
-
-        init {
-            vinylNameTextView = view.findViewById(R.id.vinyl_name_text_view)
-            vinylAuthorTextView = view.findViewById(R.id.vinyl_author_name_text_view)
-            vinylImageView = view.findViewById(R.id.imageView)
-        }
+    class ViewHolder(private val binding: VinylItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        val vinylNameTextView: TextView = binding.vinylNameTextView
+        val vinylAuthorTextView: TextView = binding.vinylAuthorNameTextView
+        val vinylImageView: ImageView = binding.imageView
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.vinyl_item, parent, false)
-        return ViewHolder(view)
+        val binding = VinylItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
