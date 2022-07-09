fded4c7a117f7693db0c8cc924c093355911c6fd
Vladislav Gladkiy <v.a.gladkiy1@gmail.com>
2022-07-09 08:35:17 +0300

    Move default parameters to the end

--- app/src/main/java/com/android/vinylstore/lastfm_api/interfaces/AlbumsApiService.kt
+++ app/src/main/java/com/android/vinylstore/lastfm_api/interfaces/AlbumsApiService.kt
@@ -9,22 +9,21 @@ import retrofit2.http.Query
 interface AlbumsApiService {
     @GET("/2.0/")
     fun getTopArtists(
-        @Query("method") method: String = "chart.gettopartists",
-        @Query("format") format: String = "json",
         @Query("api_key") apiKey: String,
         @Query("page") page: Int? = null,
-        @Query("limit") limit: Int? = null
+        @Query("limit") limit: Int? = null,
+        @Query("format") format: String = "json",
+        @Query("method") method: String = "chart.gettopartists"
     ): Call<TopArtistResponse>
 
-    // Example: http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=cher&api_key=35a05fb4101e2310860399151c60f746&format=json
     @GET("/2.0/")
     fun getTopAlbums(
-        @Query("method") method: String = "artist.gettopalbums",
-        @Query("format") format: String = "json",
         @Query("api_key") apiKey: String,
         @Query("artist") artist: String,
         @Query("autocorrect") autoCorrect: Boolean? = null,
         @Query("page") page: Int? = null,
-        @Query("limit") limit: Int? = null
+        @Query("limit") limit: Int? = null,
+        @Query("format") format: String = "json",
+        @Query("method") method: String = "artist.gettopalbums"
     ): Call<TopAlbumsResponse>
 }
