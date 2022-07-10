ccdc55fa2c9105d918738a1e5e616f2472bd9763
Vladislav Gladkiy <gladkyvladis@yandex.com>
2022-07-10 20:31:53 +0300

    Implement animations and logic for vinyl showcase

--- app/src/main/java/com/android/vinylstore/activity/AlbumActivity.kt
+++ app/src/main/java/com/android/vinylstore/activity/AlbumActivity.kt
@@ -1,21 +1,23 @@
 package com.android.vinylstore.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import com.android.vinylstore.activity.MainActivity.Companion.lastFmApi
 import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
 import com.android.vinylstore.R
+import com.android.vinylstore.activity.MainActivity.Companion.lastFmApi
+import com.android.vinylstore.adapters.VinylPagerAdapter
 import com.android.vinylstore.databinding.ActivityAlbumBinding
-import com.android.vinylstore.databinding.AlbumItemBinding
 import com.android.vinylstore.lastfm_api.classes.Tag
 import com.android.vinylstore.lastfm_api.responses.AlbumInfoResponse
-import com.android.vinylstore.lastfm_api.responses.ArtistInfoResponse
 import com.google.android.material.chip.Chip
 import com.squareup.picasso.Picasso
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+
 
 class AlbumActivity : AppCompatActivity() {
     private var _binding: ActivityAlbumBinding? = null
@@ -34,6 +36,18 @@ class AlbumActivity : AppCompatActivity() {
 
         _binding = ActivityAlbumBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val vinylViewPagerAdapter = VinylPagerAdapter(this)
+        val metrics = windowManager.currentWindowMetrics
+        val screenWidth = metrics.bounds.width().toFloat()
+        val stopPointX = binding.albumIv.x + binding.albumIv.width
+        binding.vinylVp.setPageTransformer(
+            StackedPageTransformer(
+                screenWidth,
+                resources.displayMetrics.density
+            )
+        )
+        binding.vinylVp.adapter = vinylViewPagerAdapter
 
         albumName = intent.extras?.getString(ALBUM_NAME).toString()
         artistName = intent.extras?.getString(ARTIST_NAME).toString()
@@ -104,5 +118,40 @@ class AlbumActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         finish()
         return true
+    }
+
+    class StackedPageTransformer(private val screenWidth: Float, private val density: Float) :
+        ViewPager2.PageTransformer {
+
+        // vinyl image is 200 dp wide, so 100 is a half
+        private val halfVinylImage = 100F * density
+        private val halfVinylImageOffset = halfVinylImage / screenWidth
+
+        override fun transformPage(page: View, position: Float) {
+            if (position <= -1f) {    // [-Infinity,-1)
+                // This page is way off-screen to the left.
+                page.alpha = 0F;
+                page.translationZ = 0.0f
+
+            } else if (position <= 0f) {    // [-1,0]
+                if (position <= -halfVinylImageOffset) {
+                    page.alpha = 0F;
+                    page.translationX = 0F;
+                    page.translationZ = 0.0f
+                } else {
+                    page.alpha = 1F;
+                    page.translationX = 0F;
+                    page.translationZ = 0.0f
+                }
+            } else if (position < 1) {    // (0,1]
+                page.translationX = -position * page.width;
+                page.alpha = 1f;
+                page.translationZ = -0.01f
+            } else {    // (1,+Infinity]
+                // This page is way off-screen to the right.
+                page.alpha = 0F;
+                page.translationZ = 0.0f
+            }
+        }
     }
 }
